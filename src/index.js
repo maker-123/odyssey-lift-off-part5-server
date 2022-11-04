6cd65103584519ccbb30824005e76736a42d7371
alvin <blankb223@gmail.com>
2022-11-04 11:39:39 +0800

    remove slash

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ async function startApolloServer(typeDefs, resolvers) {
     cors: {
       credentials: true,
       origin: [
-        "https://apollo-1234.herokuapp.com/",
+        "https://apollo-1234.herokuapp.com",
         "https://studio.apollographql.com",
       ],
     },
